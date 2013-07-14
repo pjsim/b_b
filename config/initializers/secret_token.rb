5c6bde079d887a006fe0ec3e5bb877267f6e1019
pjsim <phillip.j.simmonds@gmail.com>
2013-07-14 17:12:20 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BloatedBoar::Application.config.secret_token = 'a4b3448659d560cf3bef06b58bc75e56fb1c9e298645e910c1008b52149a15aeb49dec4e3d4be89d2052815fde1bfd45fbd033cb8ecd147d77d4fd6f09cc0187'
